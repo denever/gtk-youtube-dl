5b2aeb6d6f99dad688297ecb225ee7f1789b1b2a
Giuseppe <denever@truelite.it>
2011-05-18 09:25:32 +0200

    Vecchie modifiche di cui non ho più traccia

--- gtk-youtube-dl.py
+++ gtk-youtube-dl.py
@@ -9,6 +9,10 @@ class GtkYouTube:
         builder.add_from_file('data/gtk-youtube-dl.ui')
         self.win_youtubedl = builder.get_object('win_youtubedl')
         self.txt_url = builder.get_object('txt_url')
+        self.btn_download = builder.get_object('btn_download')
+        self.btn_save = builder.get_object('btn_save')
+        self.chs_fileflv = builder.get_object('chs_fileflv')
+        self.chs_filemp3 = builder.get_object('chs_filemp3')
         builder.connect_signals(self)
 
     def show(self):
@@ -19,12 +23,22 @@ class GtkYouTube:
 
     def on_btn_download_clicked(self, widget):
         url = self.txt_url.get_text()
-        cmd = 'youtube-dl %s' % url
-        popen2.popen2(cmd)
+        flv = self.chs_fileflv.get_filename()
+        cmd = 'youtube-dl -q -o %s  "%s"' % (flv, url)
+        stdin, stdout = popen2.popen2(cmd)
 
     def on_btn_save_clicked(self, widget):
+        mp3file = self.chs_filemp3.get_filename()
+        print mp3file
         cmd = 'mplayer -dumpaudio -dumpfile %s %s' % (mp3file, flvfile)
         popen2.popen2(cmd)
+        
+    def on_txt_url_changed(self, widget):
+        self.btn_download.set_sensitive(True)
+
+    def on_chs_filemp3_changed(self, widget):
+        self.btn_save.set_sensitive(True)
+    
 
 if __name__ == '__main__':
     gui = GtkYouTube()
